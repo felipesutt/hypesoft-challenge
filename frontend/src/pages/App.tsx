c1d3d82463bfbbb3c5727dab311409041b9551ad
felipe sutt <felipesa232@gmail.com>
2025-09-04 16:38:49 -0300

    chore(frontend): ambient setup and folders architecture

---
+++ frontend/src/pages/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+import { Button } from '../components/ui/button'
+
+
+export function App() {
+
+  return (
+    <div className='bg-amber-50 text-5xl'>
+      <Button>Teste</Button>
+    </div>
+  )
+}
